c2be869c7ac43271149e2dea0305a7ea844e3bf2
Bruna <ppbruna2@gmail.com>
2015-12-14 19:04:11 -0200

    create model to address. w/ giovane

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214203920) do
+ActiveRecord::Schema.define(version: 20151214205645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string   "street"
+    t.string   "number"
+    t.string   "complement"
+    t.string   "neighborhood"
+    t.string   "city"
+    t.string   "state"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "services", force: :cascade do |t|
     t.string   "area"
