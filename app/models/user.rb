b2ba07c5b76bb7157d93fcbfa8b0d4cb232ed9f7
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2015-12-21 09:37:24 -0300

    validating presence of names (social and civil)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validate :name_presence
+
+  def name_presence
+    if social_name.blank? and civil_name.blank?
+        errors.add(:_, "É preciso informar ao menos um nome.")
+    end
+  end
 end
