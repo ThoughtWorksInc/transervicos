1d0e464f42e212204411d6a6a914995df4c29503
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2015-12-21 08:27:09 -0300

    finished devise setup for custom user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'dashboard' => 'dashboard#index'
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
