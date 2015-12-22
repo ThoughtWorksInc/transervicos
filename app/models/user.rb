92ce2f69227f1cd88fa7e571f0194acf4fc70f80
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2015-12-22 09:56:30 -0300

    validating age on sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,30 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validate :name_presence
+  validate :age
 
   validates :terms_of_service, acceptance: true
 
   def name_presence
     if social_name.blank? and civil_name.blank?
         errors.add(:_, "É preciso informar ao menos um nome.")
+    end
+  end
+
+  def age
+    today = Date.current()
+
+    puts birth_date
+    puts birth_date.class
+
+    if birth_date.blank?
+        errors.add(:__, "É preciso informar uma data de nascimento.")
+        return
+    end
+
+    if (birth_date + 18.years) < today
+        errors.add(:_, "É preciso ser maior de idade.")
+        return
     end
   end
 end
