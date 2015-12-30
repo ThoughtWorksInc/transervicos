8a65ba82732d53480c03aa4515b086177fc38593
Bruna <ppbruna2@gmail.com>
2015-12-30 12:26:37 -0200

    @brunapereira - adds Outro option at area and subarea listbox

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,15 @@
-areas  = ["saude", "direito", "educacao", "religiao", "comidas", "beleza"]
+areas  = ["Saúde", "Direito", "Educação", "Religião", "Comidas", "Beleza"]
 subareas_array = [
 				["Odontologia",  "Cirurgia Plástica ",  "Urologia ",  "Ginecologia ",  "Endocrinologia ",  "Dermatologia ",  "Cirurgia Geral ",  "Infectologia ",  "Proctologia "],
 		 		["Advocacia"],
 		 		["Preparatório Pré-Vestibular", "Ensino Médio", "Ensino Fundamental", "Ensino Superior"]
 				]
 
-areas.each do |name|
-	Area.create(area: name)
-end 
+areas.each { |name| Area.create(area: name) }
+Area.create(area: "Outro")
 
 subareas_array.each_with_index do |subareas, index|
-	subareas.each do |name| 
-		Subarea.create(subarea: name, area_id: index + 1)
-	end
+	area_id = index + 1
+	subareas.each { |name| Subarea.create(subarea: name, area_id: area_id) }
+	Subarea.create(subarea: "Outro", area_id: area_id)
 end
