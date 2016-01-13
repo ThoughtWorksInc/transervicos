3141e2306ff2eccb143838884e1baabf1636e910
Rogério Chaves <rogeriocfj@gmail.com>
2016-01-13 01:39:47 -0200

    Add styling to login page with the new auth layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
 
   def after_sign_in_path_for(resource)
     dashboard_path
@@ -13,4 +14,14 @@ class ApplicationController < ActionController::Base
 	    format.json { render :json => area.subareas }
 	  end
 	end
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "auth"
+    else
+      "application"
+    end
+  end
 end
