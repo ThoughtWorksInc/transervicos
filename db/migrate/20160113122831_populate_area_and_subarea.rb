93b17881faa019010798fbb9f288218b763a60c4
Bruna <ppbruna2@gmail.com>
2016-01-13 10:34:49 -0200

    @brunapereira & @rogeriochaves - moves pre population from areas and subareas to a migration

---
+++ db/migrate/20160113122831_populate_area_and_subarea.rb
@@ -0,0 +1,29 @@
+class PopulateAreaAndSubarea < ActiveRecord::Migration
+  def up
+    areas = {
+    	"Saúde": ["Odontologia", "Cirurgia Plástica", "Urologia", "Ginecologia", "Endocrinologia", "Dermatologia", "Cirurgia Geral", "Infectologia", "Proctologia"],
+    	"Direito": ["Advocacia"],
+    	"Educação": ["Preparatório Pré-Vestibular", "Ensino Médio", "Ensino Fundamental", "Ensino Superior"],
+    	"Religião": [],
+    	"Comidas": [],
+    	"Beleza": []
+    }
+
+    area_id = 1
+
+    areas.each do |area_name, subareas|
+    	Area.create(area: area_name)
+    	subareas.each { |subarea_name| Subarea.create(subarea: subarea_name, area_id: area_id) }
+    	Subarea.create(subarea: "Outro", area_id: area_id)
+    	area_id += 1
+    end
+
+    Area.create(area: "Outro")
+    Subarea.create(subarea: "Outro", area_id: area_id)
+  end
+
+  def down
+    Area.destroy_all
+    Subarea.destroy_all
+  end
+end
