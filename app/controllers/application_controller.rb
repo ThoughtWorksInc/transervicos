84d01e8b223a912fc997a8f4d564dc4587a5ac62
Joao Lucas Santana <jlucasps@gmail.com>
2016-01-18 11:40:12 -0200

    Removes auth.html.erb layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  layout :layout_by_resource
 
   def after_sign_in_path_for(_resource)
     dashboard_path
@@ -13,11 +12,5 @@ class ApplicationController < ActionController::Base
     respond_to do |format|
       format.json { render json: area.subareas }
     end
-  end
-
-  protected
-
-  def layout_by_resource
-      'application'
   end
 end
