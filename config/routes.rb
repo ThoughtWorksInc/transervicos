ddd0420d5bfb9b48176b733ce042c45daf7a75e1
Bruna <ppbruna2@gmail.com>
2016-01-20 16:25:19 -0200

    muda o path dos serviços para testar bug no heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope(path_names: { new: 'novo', edit: 'editar' }) do
-    resources :services, path: 'servicos'
+    resources :services
   end
 
   get 'dashboard' => 'dashboard#index', :path => 'meu-perfil'
