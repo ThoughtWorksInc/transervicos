9f383934fa90205dd39a4072c942a7470bf6f6f0
Joao Lucas Santana <jlucasps@gmail.com>
2016-01-19 23:54:34 -0200

    Show area/subarea when listing services

--- spec/views/services/index.html.erb_spec.rb
+++ spec/views/services/index.html.erb_spec.rb
@@ -1,16 +1,19 @@
-# require 'rails_helper'
-#
-# RSpec.describe 'services/index', type: :view do
-#   before(:each) do
-#     assign(:services, [
-#       create(:service, name: 'Service', description: 'Desc', phone: 'Phone'),
-#       create(:service, name: 'Service', description: 'Desc', phone: 'Phone')])
-#   end
-#
-#   it 'renders a list of services' do
-#     render
-#     assert_select 'tr>td', text: 'Service'.to_s, count: 2
-#     assert_select 'tr>td', text: 'Desc'.to_s, count: 2
-#     assert_select 'tr>td', text: 'Phone'.to_s, count: 2
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe 'services/index', type: :view do
+  before(:each) do
+    assign(:services, [
+      create(:service, name: 'Service 1', description: 'Desc 1', phone: 'Phone 1'),
+      create(:service, name: 'Service 2', description: 'Desc 2', phone: 'Phone 2')])
+  end
+
+  it 'renders a list of services' do
+    render
+    expect(rendered).to match(/Service 1/)
+    expect(rendered).to match(/Service 2/)
+    expect(rendered).to match(/Desc 1/)
+    expect(rendered).to match(/Desc 2/)
+    expect(rendered).to match(/Phone 1/)
+    expect(rendered).to match(/Phone 2/)
+  end
+end
