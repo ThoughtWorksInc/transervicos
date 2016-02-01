a974d07a4a936487644bb9882b8f06df6671089f
Bruna <ppbruna2@gmail.com>
2016-02-01 16:11:42 -0200

    corrige ofensas do rubocop

--- app/views/devise/mailer/preview/devise_mailer_preview.rb
+++ app/views/devise/mailer/preview/devise_mailer_preview.rb
@@ -1,7 +1,7 @@
-class Devise::MailerPreview < ActionMailer::Preview
-
-  def reset_password_instructions
-    Devise::Mailer.reset_password_instructions(User.first, "faketoken")
+class Devise
+  class MailerPreview < ActionMailer::Preview
+    def reset_password_instructions
+      Devise::Mailer.reset_password_instructions(User.first, 'faketoken')
+    end
   end
-
 end
