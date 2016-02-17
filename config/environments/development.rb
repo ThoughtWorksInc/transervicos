28919f88bd8e4912d4b850c8c921ba026f98e55f
Marcos Brizeno <marcos.uece.comp@gmail.com>
2016-02-17 19:02:37 -0200

    Inclui porta local nas urls de emails e configura no-reply@transervicos

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 8000 }
   config.action_mailer.perform_deliveries = false
 
   config.action_mailer.preview_path = 'app/views/devise/mailer/preview'
