d23091e0e0963c867bdd8af49430179878518c58
Geisly Conca <gconca@thoughtworks.com>
2016-03-07 11:59:11 -0300

    [Geisly] - #57 - Acrescentando helper methods para verificar se area e subarea estão nulas antes de tentar acessar seus atributos. Na view usar os novos helper methods e colocar symbols ao invés de Strings em identificadores

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -10,4 +10,12 @@ module ServicesHelper
   def should_display_owner_data(service)
     service.try(:owner_name).present?
   end
+
+  def get_selected_subarea_id(service)
+    service.subarea && service.subarea.area ? service.subarea.area.id : nil
+  end
+
+  def get_selected_subareas(service)
+    service.subarea && service.subarea.area ? service.subarea.area.subareas : []
+  end
 end
