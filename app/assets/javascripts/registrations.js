a01938874ecffd533b487e9661de1bd97e906d27
faustocv <fausto.castaneda.v@gmail.com>
2016-06-03 15:01:07 -0500

    #28 with @gabicha - Fixing registrations js functionality

--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
-  $('#new_user input').on(function() {
-    validateField(this);
-    console.log('xxxxx');
-  });
-});
+//$(document).ready(function() {
+  //$('#new_user input').focusout(function() {
+    //validateField($(this));
+    //console.log('xxxxx');
+  //});
+//});
 
 var validateField = function(input){
   var span = $('span', input.parent());
   if(isValidValue(input)){
     setMessageAndClass(span, '', 'error');
   }else{
-    setMessageAndClass(span, 'O campo é requerido', 'error-required');
+    setMessageAndClass(span, 'O campo é requerido', 'error_required');
   }
-}
+};
 
 var isValidValue = function(element) {
   return !!element.val();
-}
+};
 
 var setMessageAndClass = function(element, message, clazz) {
   element.html(message);
   var actualClazz = element.attr("class");
   element.removeClass(actualClazz);
   element.addClass(clazz);
-}
+};
