789d9a78d7068ce932e4a097a4b5fd6809f94b8c
Saúl Piña <sauljabin@gmail.com>
2016-06-27 14:19:02 -0500

    #104 - Add before action authenticate user in ListsController

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,9 @@
 class ListsController < ApplicationController
   include PaginationHelper
+  before_action :authenticate_user!
+
   RECORDS_PER_PAGE = 10
+
   def services_with_reports
     @services = Service.list_services_with_reports
     @services = paginate_services(@services, RECORDS_PER_PAGE, params[:page])
