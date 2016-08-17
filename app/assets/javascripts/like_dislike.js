b93c855066f0bcd536cdeecdcbd5f5a6ae238f22
Andriele Menegotto <amenegot@thoughtworks.com>
2016-08-17 13:25:07 -0300

    Fix like/dislike logic, now user can unlike and undislike a service

--- app/assets/javascripts/like_dislike.js
+++ app/assets/javascripts/like_dislike.js
@@ -3,41 +3,49 @@ var configureLikeDislike = function(parent){
   upvote.click(function(event){
       var target = $(event.currentTarget).closest('div.likes-container');
       var serviceId = $(target).data('service');
-
-      var endPoint = 'like';
       var currentClass = 'fa-thumbs-o-up';
       var newClass = 'fa-thumbs-up';
-     if($(target).find('i').hasClass('fa-thumbs-up')){
-         endPoint = 'dislike';
-         currentClass = 'fa-thumbs-up';
-         newClass = 'fa-thumbs-o-up';
-     }
+     
+      if($(target).find('i').hasClass('fa-thumbs-up')){
+          currentClass = 'fa-thumbs-up';
+          newClass = 'fa-thumbs-o-up';
+      }
 
-      $.ajax('/servicos/'+serviceId+'/'+endPoint, {
+      $.ajax('/servicos/'+serviceId+'/like', {
           method: 'PUT',
           success: function(data) {
+              console.log(data);
               switchClasses(target,'i',currentClass, newClass);
-              // switchClasses(target,'i','fa-thumbs-down', 'fa-thumbs-o-down');
-              updateNumbers(target, data.upvotes, data.downvotes);
-            },
-            error: function() {
-               console.log('Oops');
-            }
-      });
+              switchClasses(target,'i','fa-thumbs-down', 'fa-thumbs-o-down');
 
+              updateNumbers(target, data.upvotes, data.downvotes);
+          },
+          error: function() {
+              console.log('Oops');
+          }
+      });
   });
 
   var downvote = $(parent).find('.downvote').closest('div');
   downvote.click(function(event){
       var target = $(event.currentTarget).closest('div.likes-container');
-      console.log(target);
       var serviceId = $(target).data('service');
+
+      var currentClass = 'fa-thumbs-o-down';
+      var newClass = 'fa-thumbs-down';
+
+      if($(target).find('i').hasClass('fa-thumbs-down')){
+          currentClass = 'fa-thumbs-down';
+          newClass = 'fa-thumbs-o-down';
+      }
+
       $.ajax('/servicos/'+serviceId+'/dislike', {
           method: 'PUT',
           success: function(data) {
-              // switchClasses(target,'i','fa-thumbs-up', 'fa-thumbs-o-up');
-              switchClasses(target,'i','fa-thumbs-o-down', 'fa-thumbs-down');
-              updateNumbers(target, data.upvotes, data.downvotes);
+              console.log(data);
+              switchClasses(target,'i',currentClass, newClass);
+              switchClasses(target,'i','fa-thumbs-up', 'fa-thumbs-o-up');
+              updateNumbers(target, 0, data.downvotes);
             },
             error: function() {
                console.log('Oops');
