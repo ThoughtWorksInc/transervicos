86a2c0161ce9cb9c40b5479de62f33923c76c5be
Andriele Menegotto <amenegot@thoughtworks.com>
2016-09-13 20:12:09 -0300

    Change default :from postmaster email to report owner email

--- app/mailers/trans_mailer.rb
+++ app/mailers/trans_mailer.rb
@@ -1,7 +1,7 @@
 class TransMailer < ApplicationMailer
-  default from: ENV['MAILER_USERNAME']
+  # default from: ENV['MAILER_USERNAME']
   DEFAULT_SUBJECT = 'TRANSERVIÇOS: Serviço com Denúncia'
-  ADMIN_EMAIL = 'andriele.menegotto@gmail.com'
+  ADMIN_EMAIL = 'amenegot@thoughtworks.com'
 
   def send_mail_report(report)
     @report = report
@@ -9,6 +9,6 @@ class TransMailer < ApplicationMailer
 
     bcc_mails.push(@report.service.owner_email) if @report.service.owner_email
 
-    mail(bcc: bcc_mails, subject: DEFAULT_SUBJECT, to: ADMIN_EMAIL)
+    mail(bcc: bcc_mails, subject: DEFAULT_SUBJECT, to: ADMIN_EMAIL, from: @report.service.user.email)
   end
 end
