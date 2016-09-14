d227f69fe0c3300d826b4a85eea0a574fe57fed4
Andriele Menegotto <amenegot@thoughtworks.com>
2016-09-14 10:25:00 -0300

    Sent email report just for service owner and a copy for report owner

--- app/mailers/trans_mailer.rb
+++ app/mailers/trans_mailer.rb
@@ -1,14 +1,12 @@
 class TransMailer < ApplicationMailer
-  # default from: ENV['MAILER_USERNAME']
   DEFAULT_SUBJECT = 'TRANSERVIÇOS: Serviço com Denúncia'
-  ADMIN_EMAIL = 'amenegot@thoughtworks.com'
 
   def send_mail_report(report)
     @report = report
-    bcc_mails = [@report.email, @report.service.user.email]
+    bcc_mail = [@report.email]
 
-    bcc_mails.push(@report.service.owner_email) if @report.service.owner_email
+    bcc_mail.push(@report.service.owner_email) if @report.service.owner_email
 
-    mail(bcc: bcc_mails, subject: DEFAULT_SUBJECT, to: ADMIN_EMAIL, from: @report.service.user.email)
+    mail(bcc: bcc_mail, subject: DEFAULT_SUBJECT, to: @report.service.user.email, from: @report.service.user.email)
   end
 end
